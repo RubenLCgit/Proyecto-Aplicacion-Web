a39afc78a7015776a80d110df5f165dae2c934ba
RubenLCgit <125395496+RubenLCgit@users.noreply.github.com>
2023-04-12 11:47:57 +0200

    add: Create other methods for the menu

--- AplicacionWeb/src/main/java/com/svaleroAA2EvRubenLC/Menu.java
+++ AplicacionWeb/src/main/java/com/svaleroAA2EvRubenLC/Menu.java
@@ -8,6 +8,7 @@ import com.svaleroAA2EvRubenLC.util.Utils;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.SQLOutput;
+import java.util.List;
 import java.util.Scanner;
 
 public class Menu {
@@ -30,7 +31,8 @@ public class Menu {
             System.out.println("2: Modificar Juego");
             System.out.println("3: Buscar Juego");
             System.out.println("4: Eliminar Juego");
-            System.out.println("5: Salir");
+            System.out.println("5: Mostrar todos los juegos");
+            System.out.println("6: Salir");
             System.out.println("\n");
             System.out.print("Elige un opción: ");
             Scanner entrada = new Scanner(System.in);
@@ -39,13 +41,18 @@ public class Menu {
                     menuRegistrarJuego();
                     break;
                 case "2":
+                    menuModificarJuego();
                     break;
                 case "3":
+                    menuBuscarJuego();
                     break;
                 case "4":
                     menuBorrarJuego();
                     break;
                 case "5":
+                    menuMostrarJuegos();
+                    break;
+                case "6":
                     salir=true;
                     database.cerrarConexion();
                     break;
@@ -69,10 +76,9 @@ public class Menu {
         Juego juego = new Juego(nombre, max_jug,tipo,duracion_max);
         JuegoDAO juegoDAO = new JuegoDAO(conexion);
         try {
-            juegoDAO.registrarJuego(juego);//Capturamos aqui la excepcion lanzada por el metodo registrarJuego. Tambien podriamos volver a lanzarla a otra clase superior donde se use el metodo menuRegistrarJuego y asi sucesivamente hasta que la capturemos.
+            juegoDAO.registrarJuego(juego);//Capturamos aqui la excepción lanzada por el método registrarJuego. También podríamos volver a lanzarla a otra clase superior donde se use el metodo menuRegistrarJuego y asi sucesivamente hasta que la capturemos.
             System.out.println("Juego registrado correctamente");
         }catch (SQLException sqle){
-            sqle.printStackTrace();
             System.out.println("Fallo de Conexión con la base de datos");
         }
     }
@@ -81,11 +87,59 @@ public class Menu {
         System.out.print("Nombre del juego a borrar: ");
         String nombre = entrada.next();
         JuegoDAO juegoDAO = new JuegoDAO(conexion);
-        try {
-            juegoDAO.borrarJuego(nombre);//Capturamos aqui la excepcion lanzada por el metodo registrarJuego. Tambien podriamos volver a lanzarla a otra clase superior donde se use el metodo menuRegistrarJuego y asi sucesivamente hasta que la capturemos.
-            System.out.println("Juego borrado correctamente");
+        try {//Capturamos aqui la excepcion lanzada por el metodo registrarJuego. Tambien podriamos volver a lanzarla a otra clase superior donde se use el metodo menuRegistrarJuego y asi sucesivamente hasta que la capturemos.
+            if (!juegoDAO.existeJuego(nombre))System.out.println("El juego no esta registrado");
+            else {
+                juegoDAO.borrarJuego(nombre);
+                System.out.println("Juego borrado correctamente");
+            }
         }catch (SQLException sqle){
-            sqle.printStackTrace();
+            System.out.println("Fallo de Conexión con la base de datos");
+        }
+    }
+
+    private void menuMostrarJuegos(){
+        JuegoDAO juegoDAO = new JuegoDAO(conexion);
+        try{
+            if (!juegoDAO.mostrarJuegos().isEmpty())
+                for (int i=0;i<juegoDAO.mostrarJuegos().size();i++){
+                    System.out.println(juegoDAO.mostrarJuegos().get(i));
+                }
+            else System.out.println("No hay datos registrados");
+        }catch (SQLException sqle){
+            System.out.println("Fallo de Conexión con la base de datos");
+        }
+    }
+
+    private void menuBuscarJuego(){
+        JuegoDAO juegoDAO = new JuegoDAO(conexion);
+        System.out.print("Nombre del juego que quieres buscar (0 para ningún valor): ");
+        String condicion1 = entrada.nextLine();
+        System.out.print("Y/O Tipo de juego a buscar (0 para ningún valor): ");
+        String condicion2 = entrada.nextLine();
+        try{
+            if (!juegoDAO.filtrarJuegos(condicion1,condicion2).isEmpty())
+                for (int i=0;i<juegoDAO.filtrarJuegos(condicion1,condicion2).size();i++){
+                    System.out.println(juegoDAO.filtrarJuegos(condicion1,condicion2).get(i));
+                }
+            else System.out.println("Juegos no encontrados");
+        }catch(SQLException sqle){
+            System.out.println("Fallo de Conexión con la base de datos");
+        }
+    }
+
+    private void menuModificarJuego() {
+        JuegoDAO juegoDAO = new JuegoDAO(conexion);
+        System.out.print("Nombre del juego que quieres modificar: ");
+        String nombre = entrada.nextLine();
+        System.out.print("Especifica la duración maxima del juego: ");
+        int duracion = Integer.parseInt(entrada.nextLine());
+        try {
+            if (!juegoDAO.existeJuego(nombre))System.out.println("El juego no esta registrado");
+            else {
+                juegoDAO.modificarJuego(duracion, nombre);
+                System.out.println("Juego modificado");}
+        } catch (SQLException sqle) {
             System.out.println("Fallo de Conexión con la base de datos");
         }
     }
--- AplicacionWeb/src/main/java/com/svaleroAA2EvRubenLC/dao/JuegoDAO.java
+++ AplicacionWeb/src/main/java/com/svaleroAA2EvRubenLC/dao/JuegoDAO.java
@@ -4,7 +4,10 @@ import com.svaleroAA2EvRubenLC.models.Juego;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class JuegoDAO {
@@ -30,6 +33,52 @@ public class JuegoDAO {
         PreparedStatement statement = conexion.prepareStatement(sql);
         statement.setString(1,nombre);
         statement.executeUpdate();
+        //return statement.executeUpdate()!=0;//TODO NO FUNCIONA. SIEMPRE DEVUELVE "0" AUNQUE BORRE UN REGISTRO
+    }
+
+    public List<Juego> mostrarJuegos() throws SQLException {
+        String sql = "SELECT * FROM JUEGOS ";
+        PreparedStatement statement = conexion.prepareStatement(sql);
+        return buscarJuegos(statement);
+    }
+
+    public List<Juego> filtrarJuegos(String condicion1, String condicion2) throws SQLException {
+        String sql = "SELECT * FROM JUEGOS WHERE NOMBRE = ? OR TIPO = ?";//TODO NO FUNCIONA SI METO "WHERE" COMO UN PARAMETRO "?"
+        PreparedStatement statement = conexion.prepareStatement(sql);
+        statement.setString(1,condicion1);
+        statement.setString(2,condicion2);
+        return buscarJuegos(statement);
+    }
+
+    public void modificarJuego(int duracion, String nombre) throws SQLException {
+        String sql = "UPDATE JUEGOS SET DURACION_MIN = ? WHERE NOMBRE = ?";
+        PreparedStatement statement = conexion.prepareStatement(sql);
+        statement.setInt(1,duracion);
+        statement.setString(2,nombre);
+        statement.executeUpdate();
+    }
+
+    private List<Juego> buscarJuegos(PreparedStatement statement) throws SQLException {
+        ResultSet resulset = statement.executeQuery();// Resulset contiene un cursor de una base de datos. Variable u objeto que en base de datos almacena más de un valor.
+        List<Juego>listaJuegos=new ArrayList<Juego>();
+        while (resulset.next()){//Esto comprueba que haya datos una fila tras otra de la tabla seleccionada dentro del resulset.
+            Juego juego= new Juego();
+            juego.setId_jue_pk(resulset.getString("ID_JUEGO"));
+            juego.setNombre(resulset.getString("NOMBRE"));
+            juego.setMax_jug(resulset.getInt("MAX_JUGADORES"));
+            juego.setDuracion_max(resulset.getInt("DURACION_MIN"));
+            juego.setTipo(resulset.getString("TIPO"));
+            listaJuegos.add(juego);
+        }
+        return listaJuegos;
+    }
+
+    public boolean existeJuego(String nomJuego) throws SQLException {
+        String sql = "SELECT ID_JUEGO FROM JUEGOS WHERE NOMBRE= ?";
+        PreparedStatement statement = conexion.prepareStatement(sql);
+        statement.setString(1, nomJuego);
+        ResultSet result = statement.executeQuery();
+        return result.next();
     }
 
 }
